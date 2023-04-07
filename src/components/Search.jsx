81decc30c8007fc6ffe6f9b05a85be4847af0b98
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-07 18:37:56 +0100

    added useState to manage search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 import "../styles/Search.css";
+import { ReactComponent as SearchIcon } from "../assets/searchIcon.svg";
 
 export default function Search() {
+  const [value, setValue] = useState("");
+
+  const changeValue = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
-      <input className="search--input-text" type="text" />
+      <form action="" className="search--form">
+        <input
+          className="search--input-text"
+          type="text"
+          onChange={changeValue}
+          value={value}
+        />
+        <button type="submit" className="search--btn">
+          <SearchIcon />
+        </button>
+      </form>
     </>
   );
 }
