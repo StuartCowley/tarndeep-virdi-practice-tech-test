5fd3bf75398644c57d3ecdd059c1c3ed02a37c6e
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-09 16:14:41 +0100

    fixed jest tests

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -3,13 +3,13 @@ import "../styles/SearchResults.css";
 
 export default function SearchResults({ results }) {
   let display = results.map((result) => {
-    return <img className="result--image" src={result} alt="" />;
+    return <img className="result--image" src={result} alt="space" />;
   });
 
   return (
     <>
       {results && results.length === 0 ? (
-        <p>No Results</p>
+        <p className="result--none">No Results</p>
       ) : (
         <div className="result--table">{display}</div>
       )}
