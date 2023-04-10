a185cb4d0ed462ca32c23cbfa75d4d8d32d7d471
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-04-10 13:47:32 +0100

    fixed search results component tests

--- src/___tests___/SearchResults.test.js
+++ src/___tests___/SearchResults.test.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SearchResults from "../components/SearchResults";
 
 describe("SearchResults component", () => {
+  const validProps = [
+    ["test-url-1", "alt-1"],
+    ["test-url-2", "alt-2"],
+    ["test-url-3", "alt-3"],
+    ["test-url-4", "alt-4"],
+  ];
+
   it("renders correctly", () => {
-    const { asFragment } = render(<SearchResults />);
+    const { asFragment } = render(<SearchResults results={validProps} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders image correctly", () => {
     const result = [
-      "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      [
+        "https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        "Image of the Moon",
+      ],
     ];
-    const { getByAltText } = render(<SearchResults results={result} />);
+    render(<SearchResults results={result} />);
 
-    // const imgElement = getByAltText("");
-    // expect(imgElement).toBeInTheDocument();
+    expect(screen.getByAltText("Image of the Moon")).toBeInTheDocument();
   });
 
   it("renders 'No Results' message when there are no search results", () => {
-    const { getByText } = render(<SearchResults results={[]} />);
-    // const messageElement = getByText("No Results");
-    // expect(messageElement).toBeInTheDocument();
+    render(<SearchResults results={[]} />);
+
+    expect(screen.getByText("No Results")).toBeInTheDocument();
   });
 });
